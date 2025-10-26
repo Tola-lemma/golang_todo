fa7c3b9f730b26a4fe36202cc935d9369ae56823
Tola-Lemma <tolalemma@gmail.com>
2025-10-26 22:35:21 +0300

    Feat:Logger created and used inside route'

--- Route/route.go
+++ Route/route.go
@@ -1,1 +1,13 @@
 package route
+
+import (
+	"github.com/gorilla/mux"
+    middleware "github.com/Tola-lemma/golang_todo/Middleware"
+)
+
+func InitRoutes() *mux.Router{
+	r:=mux.NewRouter()
+	r.Use(middleware.Logger)
+	
+	return  r
+}
