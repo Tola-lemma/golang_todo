808a83eae131cf0c3e2632bd7c93b88989db068a
Tola-Lemma <tolalemma@gmail.com>
2025-10-26 21:18:30 +0300

    feat:Connection to DB success

--- Database/connection.go
+++ Database/connection.go
@@ -1,4 +1,38 @@
 package database
-func InitConfig(){
-	
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+    _ "github.com/lib/pq"   
+	"github.com/spf13/viper"
+)
+var DB *sql.DB
+func InitConfig() {
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err !=nil{
+		log.Printf("Config File not found: %v",err)
+	}
+}
+func InitDB(){
+	InitConfig()
+	connStr:=fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		viper.GetString("DB_HOST"),
+		viper.GetString("DB_PORT"),
+		viper.GetString("DB_USER"),
+		viper.GetString("DB_PASSWORD"),
+		viper.GetString("DB_NAME"),
+		viper.GetString("SSL_MODE"),
+	)
+	var err error
+	DB, err = sql.Open("postgres",connStr)
+	 if err !=nil {
+		log.Fatalf("Failed to Connect to DB: %v",err)
+	 }
+	 if err = DB.Ping(); err !=nil{
+		log.Fatalf("DB not reachable: %v",err)
+	 }
+	 log.Println("Connected to PostgreSQL!")
 }
--- main.go
+++ main.go
@@ -1,4 +1,7 @@
 package main
-func main(){
 
+import database "github.com/Tola-lemma/golang_todo/Database"
+
+func main() {
+	database.InitDB()
 }
