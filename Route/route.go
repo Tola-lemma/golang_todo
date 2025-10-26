a372e547bd2d31a60f5c418ff6c0f1317a7d1998
Tola-Lemma <tolalemma@gmail.com>
2025-10-26 23:06:28 +0300

    Feat:Get-todos controller api endpoint route implement

--- Route/route.go
+++ Route/route.go
@@ -1,13 +1,15 @@
 package route
 
 import (
+	controller "github.com/Tola-lemma/golang_todo/Controller"
+	middleware "github.com/Tola-lemma/golang_todo/Middleware"
 	"github.com/gorilla/mux"
-    middleware "github.com/Tola-lemma/golang_todo/Middleware"
 )
 
 func InitRoutes() *mux.Router{
 	r:=mux.NewRouter()
 	r.Use(middleware.Logger)
-	
+	api:=r.PathPrefix("/api").Subrouter()
+	api.HandleFunc("/todos",controller.GetTodos).Methods("GET")
 	return  r
 }
