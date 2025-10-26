47f37da702a6cc7ad08034cc4a9327c7c3ff8ee6
Tola-Lemma <tolalemma@gmail.com>
2025-10-26 23:08:06 +0300

    Feat:todo_title Validator Added

--- Validations/todo_validator.go
+++ Validations/todo_validator.go
@@ -1,1 +1,18 @@
-package validations
+package Validations
+
+import (
+	"errors"
+	"strings"
+)
+
+type TodoInput struct {
+	Title string `json:"title"`
+}
+
+func ValidateTodoInput(t TodoInput) error {
+	t.Title = strings.TrimSpace(t.Title)
+	if len(t.Title) < 3 {
+		return errors.New("title must be at least 3 characters long")
+	}
+	return nil
+}
