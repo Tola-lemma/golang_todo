d4dadedd02ee0e8288ee1d229a245325f8d0514a
Tola-Lemma <tolalemma@gmail.com>
2025-10-27 20:07:34 +0300

    Feat:Update and delete functionality added

--- Controller/todo_controller.go
+++ Controller/todo_controller.go
@@ -7,7 +7,7 @@ import (
 	Database "github.com/Tola-lemma/golang_todo/Database"
 	models "github.com/Tola-lemma/golang_todo/Models"
 	"github.com/Tola-lemma/golang_todo/Validations"
-	// "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 func GetTodos(w http.ResponseWriter, r *http.Request){
@@ -50,4 +50,53 @@ func CreateTodo(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(map[string]string{"message": "Todo created successfully"})
+}
+
+// --- UPDATE TODO ---
+func UpdateTodo(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+
+	var input Validations.TodoInput
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		http.Error(w, "Invalid JSON body", http.StatusBadRequest)
+		return
+	}
+
+	if err := Validations.ValidateTodoInput(input); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	result, err := Database.DB.Exec("UPDATE todos SET title=$1 WHERE id=$2", input.Title, id)
+	if err != nil {
+		http.Error(w, "Failed to update todo", http.StatusInternalServerError)
+		return
+	}
+
+	rowsAffected, _ := result.RowsAffected()
+	if rowsAffected == 0 {
+		http.Error(w, "Todo not found", http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{"message": "Todo updated successfully"})
+}
+
+// --- DELETE TODO ---
+func DeleteTodo(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+
+	result, err := Database.DB.Exec("DELETE FROM todos WHERE id=$1", id)
+	if err != nil {
+		http.Error(w, "Failed to delete todo", http.StatusInternalServerError)
+		return
+	}
+
+	rowsAffected, _ := result.RowsAffected()
+	if rowsAffected == 0 {
+		http.Error(w, "Todo not found", http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{"message": "Todo deleted successfully"})
 }
